bb7dda9c40ad9cfbac7e2db223ba052b8731e3f1
Jean-Claude Wippler <jc@wippler.nl>
2018-03-18 16:38:28 +0100

    use asymmetric blink

--- template_stm32.c
+++ template_stm32.c
@@ -1,6 +1,13 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
+void littleDelay(void) {
+    /* wait a little bit */
+    for (int i = 0; i < LITTLE_BIT; i++) {
+        __asm__("nop");
+    }
+}
+
 int main(void) {
         rcc_periph_clock_enable(RCC_LED1);
 #if defined(STM32F1) /* F1 is a precious snowflake */
@@ -8,17 +15,17 @@ int main(void) {
 #else /* everyone else is sane */
         gpio_mode_setup(PORT_LED1, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, PIN_LED1);
 #endif
-	gpio_set(PORT_LED1, PIN_LED1);
 #if defined(RCC_LED2)
         rcc_periph_clock_enable(RCC_LED2);
         gpio_mode_setup(PORT_LED2, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, PIN_LED2);
 #endif
 	while(1) {
-		/* wait a little bit */
-		for (int i = 0; i < LITTLE_BIT; i++) {
-			__asm__("nop");
-		}
-		gpio_toggle(PORT_LED1, PIN_LED1);
+                gpio_set(PORT_LED1, PIN_LED1);
+                littleDelay();
+		gpio_clear(PORT_LED1, PIN_LED1);
+                littleDelay();
+                littleDelay();
+                littleDelay();
 #if defined(RCC_LED2)
 		gpio_toggle(PORT_LED2, PIN_LED2);
 #endif
