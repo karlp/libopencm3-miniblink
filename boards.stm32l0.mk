0786b9895abcbc8c860ed44cb98b8257ec373d3a
Jean-Claude Wippler <jc@wippler.nl>
2018-03-18 19:16:37 +0100

    fix blink rate

--- boards.stm32l0.mk
+++ boards.stm32l0.mk
@@ -2,7 +2,7 @@
 # 1) add a rule for your board, following the style in place.
 
 # STM32L0 starts up with MSI at 2.1Mhz
-STM32L0_CFLAGS=$(M0P_FLAGS) -DSTM32L0 -DLITTLE_BIT=10000 $(LFLAGS) -lopencm3_stm32l0
+STM32L0_CFLAGS=$(M0P_FLAGS) -DSTM32L0 -DLITTLE_BIT=100000 $(LFLAGS) -lopencm3_stm32l0
 
 define stm32l0board
 	$(call MakeBoard,$(1),$(2),$(3),$(STM32L0_CFLAGS),stm32l0)
