18f227a30642aab503c9f1a2247899af4c921138
Jean-Claude Wippler <jc@wippler.nl>
2018-03-18 19:16:16 +0100

    add more nucleo and olimex boards

--- boards.stm32l1.mk
+++ boards.stm32l1.mk
@@ -13,4 +13,3 @@ $(eval $(call stm32l1board,32l152cdiscovery,GPIOB,GPIO6))
 $(eval $(call stm32l1board,stm32l-discovery,GPIOB,GPIO6))
 $(eval $(call stm32l1board,nucleo-l152re,GPIOA,GPIO5))
 $(eval $(call stm32l1board,32l100cdiscovery,GPIOB,GPIO6))
-
